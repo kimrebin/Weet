6a45716d3ca3f38ffb29bf8d4a2fe7f164317bba
kimrebin <rebina0217@gmail.com>
2025-05-29 00:46:58 +0900

    gradle overwrite

--- app/src/main/java/com/example/weet/domain/model/Person.kt
+++ app/src/main/java/com/example/weet/domain/model/Person.kt
@@ -1,9 +1,8 @@
 package com.example.weet.domain.model
 
 data class Person(
-    val id: Int,
-    val name: String,
-    val photoUrl: String?,
-    val tag: String,
-    val score: Int
+    val name: String = "",
+    val tag: String = "",
+    val score: Int = 0,
+    val image: String = ""
 )
