bacfb0647092904cdd2b7eba3da6691cf6074932
kimrebin <rebina0217@gmail.com>
2025-05-29 00:51:58 +0900

    Firebase Manager Draft Completed

--- app/src/main/java/com/example/weet/data/remote/FirebaseManager.kt
+++ app/src/main/java/com/example/weet/data/remote/FirebaseManager.kt
@@ -1,10 +1,58 @@
 package com.example.weet.data.remote
-
+import android.net.Uri
+import com.example.weet.domain.model.Person
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.QuerySnapshot
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.ktx.storage
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.tasks.await
+
 import javax.inject.Inject
+import kotlin.js.ExperimentalJsFileName
 
 object FirebaseManager {
-    private val db = FirebaseFirestore.getInstance()
+    private val firestore: FirebaseFirestore by lazy {FirebaseFirestore.getInstance()}
+    private val storage: FirebaseStorage by lazy {FirebaseStorage.getInstance()}
 
-    fun getPersonsCollection() = db.collection("persons")
+    suspend fun savePerson(personId: String, person: Person) {
+        firestore.collection("persons")
+            .document(personId)
+            .set(person)
+            .await()
+    }
+
+    suspend fun getPersonByTag(tag: String): List<Person>{
+        val snapshot = firestore.collection("persons")
+            .whereEqualTo("tag", tag)
+            .get()
+            .await()
+        return snapshot.documents.mapNotNull { document: DocumentSnapshot -> document.toObject(Person::class.java) }
+    }
+
+    suspend fun getPerson(personId: String): Person? {
+        val doc = firestore.collection("persons")
+            .document(personId)
+            .get()
+            .await()
+        return doc.toObject(Person::class.java)
+    }
+
+    suspend fun deletePerson(personId: String) {
+        firestore.collection("persons")
+            .document(personId)
+            .delete()
+            .await()
+    }
+
+    suspend fun uploadProfileImage(fileUri: Uri, fileName: String): String? {
+        val ref = storage.reference.child("profile_images/$fileName")
+        ref.putFile(fileUri).await()
+        return ref.downloadUrl.await().toString()
+    }
+
 }
+//data에 담아야 할 객체 : tag, name, score, image
