5fab3fadb1c9394964b4b28ac79a0007f87dae53
kimrebin <rebina0217@gmail.com>
2025-06-17 20:42:34 +0900

    Added MainActivity

--- app/src/main/java/com/example/weet/ui/nevigation/AppNavHost.kt
+++ app/src/main/java/com/example/weet/ui/nevigation/AppNavHost.kt
@@ -7,7 +7,6 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.example.weet.data.local.SettingsKeys
 import com.example.weet.ui.screen.add.AddPersonScreen
-import com.example.weet.ui.screen.auth.AuthScreen
 import com.example.weet.ui.screen.checklist.ChecklistScreen
 import com.example.weet.ui.screen.main.MainScreen
 import com.example.weet.ui.screen.profile.ProfileScreen
@@ -15,20 +14,12 @@ import com.example.weet.ui.screen.settings.SettingsScreen
 
 
 @Composable
-fun AppNavHost(navController: NavHostController, isAuthenticated: Boolean) {
+fun AppNavHost(navController: NavHostController) {
     NavHost(
         navController = navController,
-        startDestination = if (isAuthenticated) "main" else "auth"
+        startDestination = "main"
     ){
-        composable("auth") {
-            AuthScreen(
-                onAuthSuccess = {
-                    navController.navigate("main"){
-                        popUpTo("auth") { inclusive = true }
-                    }
-                }
-            )
-        }
+
 
         composable("main") {
             MainScreen(
